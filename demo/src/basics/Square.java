cfafb0758f3d26601f551314847707a81e2bc507
srikanthpragada <srikanthpragada@gmail.com>
2025-09-29 18:58:43 +0530

    29-SEP

---
+++ demo/src/basics/Square.java
@@ -0,0 +1,19 @@
+package basics;
+
+import java.util.Scanner;
+
+public class Square {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("Enter a number :");
+		int num = s.nextInt();
+		
+		//System.out.println(num * num);
+		System.out.printf("Square = %d", num * num);
+		
+		s.close();
+	}
+
+}
