9340bd4db56570183ff083e467fea43a67c11239
srikanthpragada <srikanthpragada@gmail.com>
2025-10-07 19:56:15 +0530

    07-OCT

---
+++ demo/src/oop/FinalTest.java
@@ -0,0 +1,19 @@
+package oop;
+
+class FinalDemo {
+	private final int taxRate;
+
+	public FinalDemo(int taxRate) {
+		this.taxRate = taxRate;
+	}
+	
+}
+
+public class FinalTest {
+
+	public static void main(String[] args) {
+		 
+
+	}
+
+}
--- demo/src/oop/TestProduct.java
+++ demo/src/oop/TestProduct.java
@@ -1,49 +1,60 @@
 package oop;
 
-class Product  {
-	 private String name;
-	 private double price;
-	 private int qoh;
-	 
-	 // constructor 
-	 public Product(String n, double p) {
-		 name = n;
-		 price = p;
-	 }
-	 
-	 public void puchase(int qty) {
-		 qoh += qty;
-	 }
-	 
-	 public void sell(int qty) {
-		 qoh -= qty;
-	 }
-	 
-	 public double getNetPrice() {
-		 return price + price * 0.08; 
-	 }
-	 
-	 public void setPrice(double p) {
-		 price = p;
-	 }
-	 
-	 public int getQoh() {
-		 return qoh;
-	 }
+class Product {
+	// Instance variables
+	private String name;
+	private double price;
+	private int qoh;
+
+	// Static or class variable
+	private static double taxRate = 0.10;
+
+	// constructor
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public void puchase(int qty) {
+		this.qoh += qty;
+	}
+
+	public void sell(int qty) {
+		this.qoh -= qty;
+	}
+
+	public double getNetPrice() {
+		return this.price + this.price * Product.taxRate;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQoh() {
+		return this.qoh;
+	}
+
+	public static double getTaxRate() {
+		return Product.taxRate;
+	}
 }
 
 public class TestProduct {
 
 	public static void main(String[] args) {
-		Product p = new Product("iPhone 17 Pro", 110000);
 		
+		System.out.println(Product.getTaxRate());
+		
+		Product p = new Product("iPhone 17 Pro", 110000);
+
 		p.puchase(10);
 		p.sell(2);
-		
+
 		System.out.println(p.getNetPrice());
-		
-		//Product p2 = new Product();
-		
+
+		// Product p2 = new Product();
+
 	}
 
 }
