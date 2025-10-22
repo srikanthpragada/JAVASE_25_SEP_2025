96647d63a56ff82abee7fd5be630cdaddc3d67f0
srikanthpragada <srikanthpragada@gmail.com>
2025-10-22 18:55:58 +0530

    22-OCT

--- demo/src/library/Member.java
+++ demo/src/library/Member.java
@@ -1,5 +1,5 @@
 package library;
 
 public class Member {
-
+    BookIssue bookIssue;
 }
--- demo/src/test/Testing.java
+++ demo/src/test/Testing.java
@@ -1,12 +1,22 @@
 package test;
 
 import library.*;
+import java.util.*;
+
 
 public class Testing {
 
 	public static void main(String[] args) {
 		 Book b = new Book();
 		 Member m = new Member();
+		 
+		 // non-public class BookIssue cannot be accessed
+		 //BookIssue bi;
+		 
+		 // Classes in default package cannot be accessed 
+		 // Employee e;
+		 
+		 
 		 
 
 	}
