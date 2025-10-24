ecab5958d1e1d1600edcfd415c9aa2da930e2c4f
srikanthpragada <srikanthpragada@gmail.com>
2025-10-24 18:56:51 +0530

    24-OCT

---
+++ demo/src/oop2/CheckedUnchecked.java
@@ -0,0 +1,14 @@
+package oop2;
+
+public class CheckedUnchecked {
+
+	public static void main(String[] args) {
+		 
+		int a = Integer.parseInt("123");
+		
+		//FileReader fr = new FileReader("abc.txt");
+		 
+        
+	}
+
+}
--- demo/src/oop2/ExceptionDemo.java
+++ demo/src/oop2/ExceptionDemo.java
@@ -9,11 +9,13 @@ public class ExceptionDemo {
 			int n = Integer.parseInt(s);
 			System.out.println(100 / n);
 		} catch (NumberFormatException ex) {
-            System.out.println("Sorry! Not a valid number!");
+			System.out.println("Sorry! Not a valid number!");
+		} catch (ArithmeticException ex) {
+			System.out.println("Sorry! Zero is not a valid number!");
 		}
 
 		System.out.println("The End");
-		
+
 	}
 
 }
